19b25e0413fd6ba343c3de9c3724eb0e8838e108
Алексей <agragregra@ya.ru>
2025-02-02 12:32:39 +0300

    Update

--- js/app.js
+++ js/app.js
@@ -1,17 +1,8 @@
-// Custom Scripts
+// LIBRARIES IMPORT SECTION
 
+import '../libs/light-dark/light-dark.js'
 
+// USER SCRIPTS
 
-// Tailwind Theme Mode Switch with localStorage
+document.body.hidden = false // test
 
-const switchButtons = document.querySelectorAll('[data-mode]')
-const setTheme = (mode) => {
-	localStorage.theme = mode
-	const systemTheme = matchMedia('(prefers-color-scheme: dark)')
-	const applyTheme = () => document.documentElement.className = mode === 'auto' ? (systemTheme.matches ? 'dark' : 'light') : mode
-	systemTheme.onchange = mode === 'auto' ? applyTheme : null
-	switchButtons.forEach(btn => btn.classList.toggle('active', btn.dataset.mode === mode))
-	applyTheme()
-}
-document.addEventListener('click', ({ target }) => target.closest('[data-mode]')?.dataset.mode && setTheme(target.closest('[data-mode]').dataset.mode))
-setTheme(localStorage.theme || 'auto')
---
+++ libs/light-dark/light-dark.js
@@ -0,0 +1,11 @@
+const switchButtons = document.querySelectorAll('[data-mode]')
+const setTheme = (mode) => {
+	localStorage.theme = mode
+	const systemTheme = matchMedia('(prefers-color-scheme: dark)')
+	const applyTheme = () => document.documentElement.className = mode === 'auto' ? (systemTheme.matches ? 'dark' : 'light') : mode
+	systemTheme.onchange = mode === 'auto' ? applyTheme : null
+	switchButtons.forEach(btn => btn.classList.toggle('active', btn.dataset.mode === mode))
+	applyTheme()
+}
+document.addEventListener('click', ({ target }) => target.closest('[data-mode]')?.dataset.mode && setTheme(target.closest('[data-mode]').dataset.mode))
+setTheme(localStorage.theme || 'auto')
