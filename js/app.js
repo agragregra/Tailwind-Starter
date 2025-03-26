917ddbb3150d40b0ceb15119f134ce80d2439fae
Алексей <agragregra@ya.ru>
2025-03-26 09:42:13 +0300

    Update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
+// Custom Scripts
+
+
+
+// Tailwind Theme Mode Switch with localStorage
+
 const html = document.documentElement, mode = localStorage.getItem('mode') || 'auto'
-
 const getPreferredMode = () => matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-
 const switchMode = mode => {
   const newMode = mode === 'auto' ? getPreferredMode() : mode
   html.style.colorScheme = mode === 'auto' ? 'light dark' : newMode
@@ -10,11 +14,8 @@ const switchMode = mode => {
   localStorage.setItem('mode', mode)
   document.querySelectorAll('[data-mode]').forEach(el => el.classList.toggle('active', el.dataset.mode === mode))
 }
-
 matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
   if (localStorage.getItem('mode') === 'auto') switchMode('auto')
 })
-
 document.addEventListener('click', e => e.target.dataset.mode && switchMode(e.target.dataset.mode))
-
 switchMode(mode)
